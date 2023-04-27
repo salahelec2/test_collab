f8018b49e5acca8274663686118e617e147e2565
salahelec2 <salahelec2@gmail.com>
2023-04-27 20:18:51 +0100

    alx synced

--- main.h
+++ main.h
@@ -7,17 +7,25 @@
 #include <stdarg.h>
 #include <limits.h>
 
+/**
+ * struct print - defines a structure for symbols and print functions
+ *
+ * @type: formating type
+ * @f: The function associated
+ */
+
 typedef struct print
 {
 	char *type;
 	int (*f)(va_list);
 } print_sym_func;
 
+print_sym_func *init_print_sym(void);
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_string(va_list);
 int print_integer(va_list);
 int _print_aux(const char *format, va_list args, print_sym_func *print_sym);
-print_sym_func *init_print_sym(void);
 
 #endif /* MAIN_H */
