ca675fbda2bab4738b544a07523b9e8195eea229
salahelec2 <salahelec2@gmail.com>
2023-05-23 19:53:48 +0100

    shell project

--- main.h
+++ main.h
@@ -2,31 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdarg.h>
-#include <limits.h>
 
-/**
- * struct print - defines a structure for symbols and print functions
- *
- * @type: formating type
- * @f: The function associated
- */
-
-typedef struct print
-{
-	char *type;
-	int (*f)(va_list);
-} print_sym_func;
-
-print_sym_func *init_print_sym(void);
-
-int _printf(const char *format, ...);
-int _putchar(char c);
-int print_string(va_list);
-int print_integer(va_list);
-int print_binary(va_list);
-int _print_aux(const char *format, va_list args, print_sym_func *print_sym);
-
-#endif /* MAIN_H */
+#endif
---
+++ shell.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int main(int ac, char **argv)
+{
+	char *prompt = "Shell -> ";
+	char *ptr_txt = NULL, *txt_copy = NULL;
+	size_t n = 0;
+	ssize_t charNB;
+	const char *delim = " \n"
+
+	(void)ac;
+	(void)argv;
+
+	while (1)
+	{
+		printf("%s", prompt);
+		charNB = getline(&ptr_txt, &n, stdin);
+		if (charNB == -1)
+		{
+			printf("Exiting shell....\n");
+			return (-1);
+		}
+
+		printf("%s\n", ptr_txt);
+
+		free(ptr_txt);
+	}
+
+	return (0);
+}
