4ccc1a837deef07bdc6804f7df02316cd18c554c
bensbahou <younesbensbahou@gmail.com>
2023-05-24 20:05:35 +0100

    simple promt

--- shell.c
+++ shell.c
@@ -1,65 +1,23 @@
 #include "main.h"
 
+/**
+ * main - entry point of the shell
+ *
+ * Return: 0 on success
+ */
 
-int main(int ac, char *argv[])
+int main(void)
 {
-    char *prompt = ">> ";
-    char *lineptr = NULL, *lineptr_copy = NULL;
-    size_t n = 0;
-    ssize_t nchars_read;
-    const char *delim = " \n";
-    int num_tokens = 0;
-    char *token;
-    int i;
-	char **args;
+	char *prompt = ">> ";
+	char *read_line = NULL;
+	size_t n = 0;
 
-    (void)ac;
-	(void)argv;
-
-    while (1) {
-        printf("%s", prompt);
-        nchars_read = getline(&lineptr, &n, stdin);
-        if (nchars_read == -1){
-            printf("Exiting shell....\n");
-            break;
-        }
-
-        lineptr_copy = malloc(sizeof(char) * nchars_read);
-        if (lineptr_copy == NULL){
-            perror("tsh: memory allocation error");
-            break;
-        }
-        strcpy(lineptr_copy, lineptr);
-        token = strtok(lineptr, delim);
-        while (token != NULL){
-            num_tokens++;
-            token = strtok(NULL, delim);
-        }
-        num_tokens++;
-        args = malloc(sizeof(char *) * num_tokens);
-        token = strtok(lineptr_copy, delim);
-
-        for (i = 0; token != NULL; i++){
-            args[i] = malloc(sizeof(char) * (strlen(token) + 1));
-            strcpy(args[i], token);
-
-            token = strtok(NULL, delim);
-        }
-        args[i] = NULL;
-        execmd(args);
-
-        for (i = 0; i < num_tokens; i++) {
-            free(args[i]);
-        }
-        free(args);
-        free(lineptr_copy);
-        lineptr_copy = NULL;
-        num_tokens = 0;
-    }
-
-    if (lineptr != NULL) {
-        free(lineptr);
-    }
-
-    return (0);
+	while (1)
+	{
+		printf("%s", prompt);
+		getline(&read_line, &n, stdin);
+		printf("%s\n", read_line);
+	}
+	free(read_line);
+	return (0);
 }
