63df59c5e5ab5f0557772b2e3ada2631ea84e9e7
bensbahou <younesbensbahou@gmail.com>
2023-05-24 20:35:08 +0100

    add strcomp

--- shell.c
+++ shell.c
@@ -11,14 +11,25 @@ int main(void)
 	char *prompt = ">> ";
 	char *read_line = NULL;
 	size_t n = 0;
+	ssize_t read_chars_count;
 
 	while (1)
 	{
 		_puts(prompt);
-		getline(&read_line, &n, stdin);
-		_puts(read_line);
-        _putchar(10);
+		read_chars_count = getline(&read_line, &n, stdin);
+		if (read_chars_count == -1)
+		{
+			_puts("Finish...\n");
+			break;
+		}
+		if (_strcmp(read_line, "exit\n") == 0)
+		{
+			free(read_line);
+			exit(0);
+		}
 
+		_puts(read_line);
+		_putchar(10);
 	}
 	free(read_line);
 	return (0);
